9e5cedf69e053801450084d3a805d53711110650
phfuark <paulohsilva2007@gmail.com>
2025-03-19 11:23:20 -0300

    19.03.2025 - User
    User model created | User serializer created | views to Register and Login User

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,2 +1,7 @@
-from django.urls import urls 
+from django.urls import path
 
+from . import views
+
+urlpatterns = [
+    path("<str:message>", views.hello_message, name="index"),
+]
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,9 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +126,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
+
+AUTH_USER_MODEL = 'api.CustomUser'
